0f5bcb266cad24ac0a57337001e6045abfa5c07b
Greg MacLellan <greg@gregmaclellan.com>
2016-09-23 02:31:26 -0400

    Move generate-icons to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var icongen = require( 'icon-gen' );
+var fs = require('fs-extra');
 
 gulp.task('package', function(cb){
     var packager = require('electron-packager')
@@ -20,6 +22,30 @@ gulp.task('package', function(cb){
             console.log("Done electron-packager", appPaths); 
             cb(err);
         });
+});
+
+gulp.task('generate-icons', function(done) {
+
+    fs.ensureDirSync("./build/icons");
+
+    icongen( './assets/logo.svg', './temp', { report: true } )
+    .then( (results) => {
+        results.forEach( (item) => {
+            if (item.endsWith("app.ico")) move(item, "./build/icon.ico")
+            else if (item.endsWith("app.icns")) move(item, "./build/icon.icns")
+            else if (item.endsWith("favicon.ico")) move(item, "./build/favicon.ico")
+            else if (item.endsWith(".png")) move(item, "./build/icons/"+item.replace(/^.*favicon-(\d+).png$/, "$1x$1.png"))
+            else console.log("Not sure what to do with {item}");
+        });
+        done();
+    })
+    .catch(done);
+
+    function move(oldname, newname) 
+    {
+        console.log("Moving", oldname, "to", newname);
+        return fs.renameSync(oldname, newname);
+    }
 });
 
 gulp.task('default', ['package']);
